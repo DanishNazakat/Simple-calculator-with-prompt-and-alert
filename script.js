4ce38ed1103deee2c77c07fb510a4efce6fe20a1
DanishNazakat <danishnazakat636@gmail.com>
2025-05-20 20:52:04 -0700

    Calcuator with prompt and alert

---
+++ script.js
@@ -0,0 +1,12 @@
+let num1 = Number(prompt("Enter your first Number"));
+let num2 = Number(prompt("Enter your Second Number"));
+
+
+
+alert(num1+num2 );
+console.log(num1+num2 );
+console.log(num1-num2 );
+console.log(num1*num2 );
+console.log(num1/num2 );
+
+
