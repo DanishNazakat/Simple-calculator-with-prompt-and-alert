57afd4451262b4de1684c8236cb8cc691adb6cd8
DanishNazakat <danishnazakat636@gmail.com>
2025-05-20 23:20:12 -0700

    Add More Operators

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
+
+alert("Add a Number")
 let num1 = Number(prompt("Enter your first Number"));
 let num2 = Number(prompt("Enter your Second Number"));
-
-
-
 alert(num1+num2 );
+alert("Subtract the Number");
+let num3 = Number(prompt("Enter your first Number"));
+let num4 = Number(prompt("Enter your Second Number"));
+alert(num3-num4 );
+
+
+alert("Multiply the Number");
+let num5 = Number(prompt("Enter your first Number"));
+let num6 = Number(prompt("Enter your Second Number"));
+alert(num5*num6 );
+
+
+alert("Divide the Number");
+let num7 = Number(prompt("Enter your first Number"));
+let num8 = Number(prompt("Enter your Second Number"));
+alert(num7/num8 );
+
 console.log(num1+num2 );
-console.log(num1-num2 );
-console.log(num1*num2 );
-console.log(num1/num2 );
+console.log(num3-num4 );
+console.log(num5*num6 );
+console.log(num7/num8 );
 
 
